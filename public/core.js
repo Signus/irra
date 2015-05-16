66566cc8853d99d3db3bb19a9079be3b96818a22
Signus <signusx7@gmail.com>
2015-05-15 19:11:17 -0600

    Update subtopic score calculation.

--- public/core.js
+++ public/core.js
@@ -21,52 +21,36 @@ function mainController($scope, $http) {
     // When a new seleciton is made, update the question in scope
     // Trigger a recalculation of the subtopic score
     $scope.subtopicScoreUpdate = function(question, valueResponse, modifier) {
-	    
-	    if (valueResponse == 1)
-	    	question.valueResponse = 1;
-        else
-			question.valueResponse = 0;
-
-	    if (modifier.toUpperCase() == "HIGH")
-	    	question.modifier = 3;
-	    else if (modifier.toUpperCase() == "MEDIUM")
-            question.modifier = 2;
-	    else if (modifier.toUpperCase() == "LOW")
-	    	question.modifier == 1;
-
-	    $scope.subtopicCalculate(question);
-  	};
-
-    // Recalulate the subtopic score on every dropdown change.
-    // TODO: Don't calculate pass/fail items.
-  	$scope.subtopicCalculate = function (question) {
-
         var total_weighted = 0;
         var total_score = 0;
+	    
+	    (valueResponse == 1) ? question.valueResponse = 1 : question.valueResponse = 0;
 
-        angular.forEach(question, function (q, v) {
-            console.log('v: ' + v);
-            q.score = q.valueResponse
-            console.log(q.score);
-            total_weighted += q.modifier;
-            console.log(total_weighted);
-            total_score += q.score;
-            console.log(total_score);
-        });
+        switch (modifier.toUpperCase()) {
+            case 'HIGH':
+                question.modifier = 3;
+                break;
+            case 'MEDIUM':
+                question.modifier = 2;
+                break;
+            case 'LOW':
+                question.modifier = 1;
+                break;
+        }
 
-        $scope.estimated_score = total_score / total_weighted;
-        //console.log($scope.estimated_score);
-        //console.log(total_score);
-        //console.log($scope.totalScore);
-        //console.log(total_weighted);
-    };
+        total_weighted += question.modifier;
+        total_score += question.valueResponse;
+
+        // FIXME: All subtopic scores update
+        // FIXME: Subtopic score reflects changes over time, resulting in wrong score
+        $scope.estimated_score = (total_score / total_weighted).toFixed(2);
+  	};
 
     // Initialize the score to 1.
+    // TODO: Set the response visible to 'Yes'
 	$scope.scoreInit = function() {
         angular.forEach($scope.domains, function(question) {
             question.valueResponse = 1;
         });
-
-        $scope.subtopicCalculate();
     }();
 }
